dfbfcd18c5e585e8a2f89b30dc0238a01af369e0
Vlad <example@gmail.com>
2022-06-18 00:43:57 +0300

    second practice

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 'use strict';
 
+// let i = 0;
+// while (i < 2) {
+//     const a = prompt('One of last watched films?', '');
+//     const b = prompt('Please, mark this film','');
+    
+//     if (a != null && b != null && a !='' && b !='' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+//     i++;
+// }
+
+// do {
+//     const a = prompt('One of last watched films?', '');
+//     const b = prompt('Please, mark this film','');
+    
+//     if (a != null && b != null && a !='' && b !='' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+//     i++;
+// } while (i < 2);
+
 const numberOfFilms = +prompt('How many films you watched?', '');
 
 const personalMovieDB = {
@@ -10,12 +39,27 @@ const personalMovieDB = {
     privat: false
 };
 
-const firstFilmName = prompt('One of last watched films?', '');
-const firstFilmMark = prompt('Please, mark this film', '1.0 to 10.0');
-const secondFilmName = prompt('One of last watched films?', '');
-const secondFilmMark = prompt('Please, mark this film', '1.0 to 10.0');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('One of last watched films?', '');
+    const b = prompt('Please, mark this film','');
+    
+    if (a != null && b != null && a !='' && b !='' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
-personalMovieDB.movies[firstFilmName] = firstFilmMark;
-personalMovieDB.movies[secondFilmName] = secondFilmMark;
+if (personalMovieDB.count < 10) {
+    console.log('You watched a few films');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('You great film-lover');
+} else if (personalMovieDB.count >= 30) {
+    console.log('You GOAT');
+} else {
+    console.log('Error');
+}
 
 console.log(personalMovieDB);
